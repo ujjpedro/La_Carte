6b27adfc7dad98f60b498ba632e9651842a25cf0
ujj.pedro <88471571+ujjpedro@users.noreply.github.com>
2022-11-17 17:28:54 -0200

    Atualização 17/11 16:28

--- La_Carte/home/carrinho.php
+++ La_Carte/home/carrinho.php
@@ -20,8 +20,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/estilos1.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet"> -->
     <title><?php $title ?></title>
     <link rel="shortcut icon" href="../img/favicon.ico">
     <script>
